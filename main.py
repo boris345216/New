2608f04397923752ec7b6ff5ed2e1dc4f7e34941
Mr.FIGER't <sergpakhomov11@gmail.com>
2021-12-09 18:53:33 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+    for i in range(len(arr)):
+        minimum = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] > arr[minimum]:
+                minimum = j
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+for i in range(n):
+    print(arr[i])
